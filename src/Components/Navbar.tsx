1d5cdfdac38e565e563bd736c21b8866f0aac550
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-05 13:52:10 +0600

    navbar component

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {useAppDispatch, useAppSelector} from "../redux/hooks";
+import {changeTheme} from "../redux/Theme/ThemeSlice";
+import {theme} from "../redux/Theme/ThemeSlice";
+const Navbar = () => {
+    const dispatch = useAppDispatch()
+    const themeState = useAppSelector(theme)
+
+    return (
+        <nav className={themeState ? 'navbar__wrapper_black' : 'navbar__wrapper'}>
+            <div className="navbar__container">
+                <div className="navbar__logo" >Notes</div>
+                <button
+                    className="navbar__button"
+                    onClick={() => dispatch(changeTheme())}
+                >Change Theme</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
