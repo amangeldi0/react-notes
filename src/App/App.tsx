22b1129f47bd98834265a7a393bec57643ed0d4e
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-05 12:43:43 +0600

    clear project

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='wrapper'>
+            <div className="wrapper__container"></div>
+        </div>
+    );
+};
+
+export default App;
