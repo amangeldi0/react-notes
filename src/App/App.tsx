4bda377de7bf5b215b2ed7a8f294ad339b78ec7a
amangeldi0 <amanimanbekov12@gmail.com>
2022-11-05 13:53:33 +0600

    import navbar

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from "../Components/Navbar";
 
 const App = () => {
     return (
         <div className='wrapper'>
-            <div className="wrapper__container"></div>
+            <div className="wrapper__container">
+                <Navbar />
+            </div>
         </div>
     );
 };
